11f158c1d3e8d85bd0e1ccff44c91997e482fb99
Enuly <emanuelle.lorrany13@gmail.com>
2024-05-11 20:20:48 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hakeei teu cell jajajahhahah</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(100,90,100)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
