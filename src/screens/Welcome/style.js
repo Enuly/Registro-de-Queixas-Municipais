51e7ac80882b52876fd3d05395f3cb5b8e23cb61
Enuly <emanuelle.lorrany13@gmail.com>
2024-05-13 18:09:44 -0300

    inseridas as imagens

--- src/screens/Welcome/style.js
+++ src/screens/Welcome/style.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
     container:{
-        borderStartColor: "#09579E",
+        backgroundColor: "#09579E",
         flex:1,
         alignItems:"center",
         justifyContent:"center"
