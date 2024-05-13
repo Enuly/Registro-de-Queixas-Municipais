747f701bece896757363e2423d05ad934f12235f
Enuly <emanuelle.lorrany13@gmail.com>
2024-05-13 18:27:13 -0300

    Inserindo imagem de Welcome

--- src/screens/Welcome/welcome.js
+++ src/screens/Welcome/welcome.js
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native"
+import { View, Text, Image} from "react-native"
 import { style } from "./style"
 export default function Welcome(){
     return(
         <View style={style.container}>
-            <Text>Tela de welcome</Text>
+            <View>
+            <Image style={{width: "100%"}} source={require("../../../assets/logo-Welcome.png")}></Image>
+            </View>
+            <View>
+                
+            </View>
         </View>
     ) 
 }
