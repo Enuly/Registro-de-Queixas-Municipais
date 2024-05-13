024337e72dd3569c159e2879ca1e7ddff0db9c7a
antfconeto <antfcone@gmail.com>
2024-05-13 18:45:54 -0300

    alteracoes

--- src/screens/Welcome/style.js
+++ src/screens/Welcome/style.js
@@ -2,9 +2,23 @@ import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
     container:{
-        backgroundColor: "#09579E",
         flex:1,
         alignItems:"center",
         justifyContent:"center"
+    },
+    containerImg:{
+        flex:2,
+        width:"100%",
+        justifyContent: 'center',
+        alignItems:"center",
+        backgroundColor:"#09579E"
+    },
+    img:{
+        resizeMode:"contain",
+        width:"60%"
+    },
+    containerDescription:{
+        backgroundColor:"white",
+        flex:1
     }
 }) 
--- src/screens/Welcome/welcome.js
+++ src/screens/Welcome/welcome.js
@@ -1,14 +1,15 @@
-import { View, Text, Image} from "react-native"
-import { style } from "./style"
-export default function Welcome(){
-    return(
-        <View style={style.container}>
-            <View>
-            <Image style={{width: "100%"}} source={require("../../../assets/logo-Welcome.png")}></Image>
-            </View>
-            <View>
-                
-            </View>
-        </View>
-    ) 
+import { View, Text, Image } from "react-native";
+import { style } from "./style";
+export default function Welcome() {
+  return (
+    <View style={style.container}>
+      <View style={style.containerImg}>
+        <Image
+          style={style.img}
+          source={require("../../../assets/logo-Welcome.png")}
+        ></Image>
+      </View>
+      <View style={style.containerDescription}></View>
+    </View>
+  );
 }
